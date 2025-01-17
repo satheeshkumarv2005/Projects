9d35a0b50415f70b009dbab2de294e6a882af8a4
satheeshkumarv2005 <satheeshkumarv2005@gmai.com>
2025-01-17 17:50:20 +0530

    changes

--- Bank/resources/views/account/statement.blade.php
+++ Bank/resources/views/account/statement.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Statement') }}</div>
+                <div class="card-header">{{ __('Statement') }} <br><a href="/home">Back</a></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,35 +15,35 @@
                         </div>
                     @endif
 
-                    <h1>Statement</h1>
-                    <style>
-                        table th,td {
-                            border: 3px solid black;
-                            border collapse: collabse;
-                            padding: 10px;
-                        }
-                    </style>
+                    <h3>Statement</h3>
 
-                    <table>
+                    <table class="table table-bordered">
                         <thead>
-                            <th>S.no</th>
-                            <th>Account name</th>
-                            <th>Amount</th>
-                            <th>Type</th>
-                            <th>Transfer To</th>
+                          <tr>
+                            <th scope="col">S.no</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Account name</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Transfer To</th>
+                            <th scope="col">Current Balance</th>
+                          </tr>
                         </thead>
                         <tbody>
-                            @foreach($statements as $statement)
+                            @php $i = 1; @endphp
+                            @foreach($transactions as $transaction)
                                 <tr>
-                                <td><h2>{{ $statement->id}}</h2></td>
-                                <td><h2>{{ $statement->name }}</h2></td>
-                                <td><h2>{{ $statement->amount }}</h2></td>
-                                <td><h2>{{ $statement->type }}</h2></td>
-                                <td><h2>{{ $statement->transfer_to}}</h2></td>
+                                    <th scope="row">{{ $i++ }}</th>
+                                    <td>{{ $transaction->created_at }}</td>
+                                    <td>{{ $transaction->name }}</td>
+                                    <td>{{ $transaction->amount }}</td>
+                                    <td>{{ $transaction->type }}</td>
+                                    <td>{{ $transaction->transfer_to }}</td>
+                                    <td>{{ $transaction->balance }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
-                    </table>
+                      </table>
                 </div>
             </div>
         </div>
